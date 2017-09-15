3faf2fc24edc0526c9ff817c5ed41b4e6b12dbc2
Sam Williams <sam.williams@Ashleys-MacBook-Pro.local>
2017-09-16 10:28:37 +1200

    Init

---
+++ Droid/Properties/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FortyTwo.Droid
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FortyTwo.Droid")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("willsam100")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ FortyTwo/FortyTwoPage.xaml.fs
@@ -0,0 +1,25 @@
+﻿namespace FortyTwo
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+// Alias for int. To help make it clear where the model is used
+type Model = int
+
+// Interface
+type IDataLoaderService = 
+    abstract member LoadData: unit -> Model
+
+// Implementation of interface
+type DataLoaderService() = 
+    interface IDataLoaderService with 
+        member this.LoadData() = 42
+
+// View model with service as a constructor argument
+type MainViewModel(_dataLoader: IDataLoaderService) = 
+    member this.Data = _dataLoader.LoadData().ToString()
+
+type FortyTwoPage() as this =
+    inherit ContentPage()
+    let _ = base.LoadFromXaml(typeof<FortyTwoPage>)
+    do this.BindingContext <- MainViewModel(DataLoaderService())
